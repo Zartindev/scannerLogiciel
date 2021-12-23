cd6cc0d759ad6d1e535bb3f57e83f22330e1d4f2
Zartinx <martinguillon.mgcode@gmail.com>
2021-12-23 20:36:00 +0100

    Final 23
    tac

--- src/logiciel/MainLog.java
+++ src/logiciel/MainLog.java
@@ -19,9 +19,11 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
@@ -280,6 +282,16 @@ public class MainLog extends Application {
 				presenceAdminButton = 1;
 			}
 		
+
+		});
+		
+		
+		scanner.setOnMouseClicked((e) -> {
+			Alert dialog = new Alert(AlertType.INFORMATION);
+			dialog.setTitle("Information d'ajout - ERREUR");
+			dialog.setHeaderText("ERREUR\nAjout non effectué.");
+			dialog.setContentText(">>>>>>>>>>En travaux<<<<<<<<<<<");
+			dialog.showAndWait();
 
 		});
 		
